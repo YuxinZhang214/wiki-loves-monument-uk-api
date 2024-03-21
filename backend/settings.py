6d17f3a221fd05ee7a91c4d4f96b580c6999c82a
YuxinZhang214 <72717144+YuxinZhang214@users.noreply.github.com>
2024-03-21 12:58:21 +0000

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -110,5 +110,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Allow all origins for CORS
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
